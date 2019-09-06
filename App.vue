2cdea3b52518bc9053d89690b63c61012a1d21dd
handongxun <handongxun@dcloud.io>
2019-09-06 14:36:45 +0800

    update: todo临时处理滑动加载逻辑，增加检查更新逻辑

--- App.vue
+++ App.vue
@@ -1,9 +1,36 @@
 <script>
 	export default {
 		onLaunch: function() {
-			console.log('App Launch')
+			console.log('App Launch');
 			// #ifdef APP-NVUE
 			plus.screen.lockOrientation('portrait-primary');
+
+			let appid = plus.runtime.appid;
+			if (appid && appid.toLocaleLowerCase() != "hbuilder") {
+				uni.request({
+					url: 'https://uniapp.dcloud.io/update', //检查更新的服务器地址
+					data: {
+						appid: plus.runtime.appid,
+						version: plus.runtime.version
+					},
+					success: (res) => {
+						console.log('success', res);
+						if (res.statusCode == 200 && res.data.isUpdate) {
+							let openUrl = plus.os.name === 'iOS' ? res.data.iOS : res.data.Android;
+							// 提醒用户更新
+							uni.showModal({
+								title: '更新提示',
+								content: res.data.note ? res.data.note : '是否选择更新',
+								success: (showResult) => {
+									if (showResult.confirm) {
+										plus.runtime.openURL(openUrl);
+									}
+								}
+							})
+						}
+					}
+				})
+			}
 
 			var domModule = weex.requireModule('dom');
 			domModule.addRule('fontFace', {
