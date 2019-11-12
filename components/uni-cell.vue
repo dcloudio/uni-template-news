4f3d850b317b8400aed6d875c58eb9db28eeaa6c
handongxun <handongxun@dcloud.io>
2019-11-12 11:40:56 +0800

    增加tab底部选中线，优化代码结构

---
+++ components/uni-cell.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- #ifdef APP-NVUE -->
+    <cell>
+        <slot />
+    </cell>
+    <!-- #endif -->
+    <!-- #ifndef APP-NVUE -->
+    <view>
+        <slot />
+    </view>
+    <!-- #endif -->
+</template>
+
+<script>
+    export default {
+        name: 'UniCell',
+        props: {}
+    }
+</script>
