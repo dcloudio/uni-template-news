abac63bb5aadf01126cfbaa595efe26390b1c60b
handongxun <handongxun@dcloud.io>
2020-09-24 21:20:22 +0800

    update: 适配大屏 HBuilderX 2.9.0+

---
+++ responsive/right-window.vue
@@ -0,0 +1,25 @@
+<template>
+  <view>
+    <pages-detail-detail ref="detailPage"></pages-detail-detail>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Hello'
+      }
+    },
+    created(e) {
+      uni.$on('updateDetail', (e) => {
+        this.$refs.detailPage.load(e.detail);
+      })
+    },
+    onLoad() {},
+    methods: {}
+  }
+</script>
+
+<style>
+</style>
