684178b5d169c231dff57a3a99090d2fe891997d
崔红保 <hbcui1984@163.com>
2020-10-19 20:34:01 +0800

    增加rightWindow示例的注释

--- responsive/right-window.vue
+++ responsive/right-window.vue
@@ -1,5 +1,7 @@
 <template>
   <view>
+	<!-- 这里将 /pages/detail/detail.nvue 页面作为一个组件使用 -->
+	<!-- 路径 “/pages/detail/detail” 转为 “pages-detail-detail” 组件 -->
     <pages-detail-detail ref="detailPage"></pages-detail-detail>
   </view>
 </template>
@@ -12,7 +14,9 @@
       }
     },
     created(e) {
+      //监听自定义事件，该事件由详情页列表的点击触发
       uni.$on('updateDetail', (e) => {
+	    // 执行 /pages/detail/detail.nvue页面的load方法
         this.$refs.detailPage.load(e.detail);
       })
     },
